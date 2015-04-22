18b957ae3b2592f50ee6c978d98590e6fb06ed48
Charles <pl497@cornell.edu>
2015-04-21 23:45:08 -0400

    with 5

--- HW2.py
+++ HW2.py
@@ -2,7 +2,126 @@ import cv2
 import numpy as np
 import sys
 import re
+from scipy import *
+from scipy.linalg import *
+from scipy.special import *
+from random import choice
+from scipy import linalg
 
+def ransac(points_list, iters = 10 , error = 10, good_model_num = 11):
+    '''
+        This function uses RANSAC algorithm to estimate the
+        shift and rotation between the two given images
+    '''
+    
+    model_error = 255
+    model_H = None
+
+    for i in range(iters):
+        consensus_set = []
+        points_list_temp = copy(points_list).tolist()
+        # Randomly select 3 points
+        for j in range(3):
+            temp = choice(points_list_temp)
+            consensus_set.append(temp)
+            points_list_temp.remove(temp)
+        
+        # Calculate the homography matrix from the 3 points
+        
+        fp0 = []
+        fp1 = []
+        fp2 = []
+        
+        tp0 = []
+        tp1 = []
+        tp2 = []
+        for line in consensus_set:
+        
+            fp0.append(line[0][0])
+            fp1.append(line[0][1])
+            fp2.append(1)
+            
+            tp0.append(line[1][0])
+            tp1.append(line[1][1])
+            tp2.append(1)
+            
+        fp = array([fp0, fp1, fp2])
+        tp = array([tp0, tp1, tp2])
+        
+        H = Haffine_from_points(fp, tp)
+                            
+        # Check if the other points fit this model
+        for p in points_list_temp:
+            x1, y1 = p[0]
+            x2, y2 = p[1]
+
+            A = array([x1, y1, 1]).reshape(3,1)
+            B = array([x2, y2, 1]).reshape(3,1)
+            
+            out = B - dot(H, A)
+            dist_err = hypot(out[0][0], out[1][0])
+            if dist_err < error:
+                consensus_set.append(p)            
+            
+
+        # Check how well is our speculated model
+        if len(consensus_set) >= good_model_num:
+            dists = []
+            for p in consensus_set:
+                x0, y0 = p[0]
+                x1, y1 = p[1]
+                
+                A = array([x0, y0, 1]).reshape(3,1)
+                B = array([x1, y1, 1]).reshape(3,1)
+                
+                out = B - dot(H, A)
+                dist_err = hypot(out[0][0], out[1][0])
+                dists.append(dist_err)
+            if (max(dists) < error) and (max(dists) < model_error):
+                model_error = max(dists)
+                model_H = H
+                        
+    return model_H
+
+def Haffine_from_points(fp,tp):
+    """ find H, affine transformation, such that 
+        tp is affine transf of fp"""
+
+    if fp.shape != tp.shape:
+        raise RuntimeError, 'number of points do not match'
+
+    #condition points
+    #-from points-
+    m = mean(fp[:2], axis=1)
+    maxstd = max(std(fp[:2], axis=1))
+    C1 = diag([1/maxstd, 1/maxstd, 1]) 
+    C1[0][2] = -m[0]/maxstd
+    C1[1][2] = -m[1]/maxstd
+    fp_cond = dot(C1,fp)
+
+    #-to points-
+    m = mean(tp[:2], axis=1)
+    C2 = C1.copy() #must use same scaling for both point sets
+    C2[0][2] = -m[0]/maxstd
+    C2[1][2] = -m[1]/maxstd
+    tp_cond = dot(C2,tp)
+
+    #conditioned points have mean zero, so translation is zero
+    A = concatenate((fp_cond[:2],tp_cond[:2]), axis=0)
+    U,S,V = linalg.svd(A.T)
+
+    #create B and C matrices as Hartley-Zisserman (2:nd ed) p 130.
+    tmp = V[:2].T
+    B = tmp[:2]
+    C = tmp[2:4]
+
+    tmp2 = concatenate((dot(C,linalg.pinv(B)),zeros((2,1))), axis=1) 
+    H = vstack((tmp2,[0,0,1]))
+
+    #decondition
+    H = dot(linalg.inv(C2),dot(H,C1))
+
+    return H / H[2][2]
 
 def findInterestPoints(filename):
 	keynames = re.split(r'\.', filename)
@@ -25,6 +144,7 @@ def findMatches(filename1, filename2):
 
 	kp_matches1 = []
 	kp_matches2 = []
+	kp_matches = []
 	des_matches = []
 	
 	for i in range(0, des1.shape[0]):
@@ -45,17 +165,18 @@ def findMatches(filename1, filename2):
 		if (best_match / second_match) < 0.8:
 			kp_matches1.append(kp1[i].pt)
 			kp_matches2.append(kp2[bm_index].pt)
-	print kp_matches1
-	print kp_matches2
-	return kp_matches1, kp_matches2, des_matches
+			kp_matches.append([kp1[i].pt, kp2[bm_index].pt])
+
+	print kp_matches
+	
+	return kp_matches1, kp_matches2, kp_matches, des_matches
 
 def showMatches(matches1, matches2, image1, image2):
 	
 	rows1 = image1.shape[0]        
 	rows2 = image2.shape[0]
 	col1 = image1.shape[1]
-	# print rows1
-	# print rows2 
+	
 	if rows1 < rows2:
 		image1 = np.concatenate((image1, np.zeros((rows2 - rows1, image1.shape[1],3))), axis = 0)
 	else:
@@ -69,9 +190,32 @@ def showMatches(matches1, matches2, image1, image2):
 	
 	return res
 
-def affineMatches:
 
-	
+def affineMatches(matches, image1, image2) :
+	print matches
+	H = ransac(matches)
+	matches_affine = []
+	matches_affine1 = []
+	matches_affine2 = []
+
+	for mat in matches:
+		vector1 = list(mat[0])
+		vector2 = list(mat[1])
+		vector1.append(1)
+		vector2_est = np.dot(H, vector1)
+		print "vector1"
+		print vector1
+		print "vector2"
+		print vector2
+		if ((vector2_est[0]/vector2[0] + vector2_est[1]/vector2[1]) > 1.99):
+			matches_affine1.append((vector1[0], vector1[1]))
+			matches_affine2.append((vector2[0], vector2[1]))
+			matches_affine.append([(vector1[0], vector1[1]),(vector2[0], vector2[1])]) 
+
+	result_image = showMatches(matches_affine1, matches_affine2, image1, image2)	
+
+	return result_image, H
+
 #def alignImages:
 #
 #def affineMatches_homography:
@@ -83,10 +227,13 @@ if __name__ == "__main__":
 	img1 = cv2.imread('StopSign1.jpg')
 	img2 = cv2.imread('StopSign2.jpg')
 
-	kps1, kps2, des_matches = findMatches('StopSign1.jpg', 'StopSign2.jpg')
+	kps1, kps2, kps, des_matches = findMatches('StopSign1.jpg', 'StopSign2.jpg')
 
-#	result_img12 = showMatches(kps1, kps2, img1, img2)
-#	cv2.imwrite('result12.png', result_img12)
+	result_img12 = showMatches(kps1, kps2, img1, img2)
+	cv2.imwrite('result12.png', result_img12)
+	
+	result_img12_affine, H = affineMatches(kps, img1, img2)
+	cv2.imwrite('result12_affine.png', result_img12_affine)
 
 #	for kp in kps12:
 #		img[kp[0]][kp[1]] = [0 ,255, 0]
